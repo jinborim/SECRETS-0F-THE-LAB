e4f8de055d90c70568006f49ed348e9eb7e693a0
jinborim <jj8638638@naver.com>
2026-05-07 13:45:46 +0900

    인벤토리 및 아이템 시스템 코드 추가

---
+++ Assets/My_Assets/Sources/Script/Behave/Add_item.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Add_item : MonoBehaviour
+{
+    public Item add_item;
+    
+
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
